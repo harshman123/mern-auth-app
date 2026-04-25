2f45551e4aef199f5cef8ba758415081346bb7f3
harshman123 <harshkumar1052002@gamil.com>
2026-04-25 20:14:39 +0530

    render api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://mern-auth-app-a8kl.onrender.com/api"
 });
 
 // interceptor → runs before every request
